035e646392455eb85b714904b4cd02c8ff21cfc6
GMatew <gabriel.olarte@correounivalle.edu.co>
2016-04-06 19:24:56 -0600

    Se agregó Interfaz
    1. Se agregó el JFrame vista
    2. Se agregó JDialog carharSudoku
    3. Se deshabilitaron los métodos cargarMatriz() y main()      de la
    clase SudokuIA
    4. El método aplicarAlgoritmoAvara() de la clase SudokuIA ahora recibe
    una matriz como parámetro.

--- src/Sudoku/SudokuIA.java
+++ src/Sudoku/SudokuIA.java
@@ -7,83 +7,82 @@ public class SudokuIA {
     HeuristicaPosibilidades heuristicaPosibilidades=new HeuristicaPosibilidades();
     HeuristicaNumerosUnicos heuristicaNumerosUnicos=new HeuristicaNumerosUnicos();
 
-    public int[][] cargarMatriz()
-    {
-        //Facil
-         /*
-        int[][] matrizActual= {
-                                new int[] {8,0,3,9,0,0,1,0,6},
-                                new int[] {0,6,0,3,0,4,0,0,0},
-                                new int[] {0,5,4,7,1,0,0,0,0},
-                                new int[] {0,4,2,0,5,0,0,0,1},
-                                new int[] {0,3,1,4,0,2,6,5,0},
-                                new int[] {9,0,0,0,6,0,2,3,0},
-                                new int[] {0,0,0,0,4,1,8,6,0},
-                                new int[] {0,0,0,5,0,8,0,9,0},
-                                new int[] {3,0,8,0,0,9,4,0,5}};
-                 */
-         
-        //Dificil
-        /*
-          int[][] matrizActual= {
-                                new int[] {0,0,0,7,0,0,0,8,0},
-                                new int[] {2,0,0,0,0,3,0,0,0},
-                                new int[] {0,4,0,0,0,0,2,0,3},
-                                new int[] {7,0,0,0,0,0,0,0,6},
-                                new int[] {0,8,5,0,0,0,0,0,0},
-                                new int[] {0,0,4,0,8,0,3,7,1},
-                                new int[] {0,7,0,0,6,8,0,0,9},
-                                new int[] {0,0,3,0,0,0,4,0,0},
-                                new int[] {0,0,2,0,5,1,0,0,0}};
-                */
-        
-        /*
-        //Muy muy dificil
-          int[][] matrizActual= {
-                                new int[] {0,9,0,0,0,1,5,0,7},
-                                new int[] {0,0,0,0,5,0,0,9,0},
-                                new int[] {0,0,8,9,0,0,2,0,0},
-                                new int[] {0,0,9,0,0,0,0,4,0},
-                                new int[] {0,2,0,0,8,0,0,6,0},
-                                new int[] {0,6,0,0,0,0,1,0,0},
-                                new int[] {0,0,2,0,0,9,3,0,0},
-                                new int[] {0,7,0,0,4,0,0,0,0},
-                                new int[] {1,0,3,5,0,0,0,8,0}
-                                };
-        */
-        
-        /*
-        //Endiablado
-          int[][] matrizActual= {
-                                new int[] {0,0,0,0,5,0,0,0,2},
-                                new int[] {7,0,0,0,4,6,0,8,0},
-                                new int[] {0,6,0,3,8,0,0,9,0},
-                                new int[] {0,1,0,0,9,5,0,0,4},
-                                new int[] {0,0,0,0,3,0,0,2,0},
-                                new int[] {0,0,0,0,1,8,0,0,9},
-                                new int[] {3,0,0,9,7,0,0,0,8},
-                                new int[] {0,0,5,0,2,0,0,0,7},
-                                new int[] {0,0,8,5,6,0,0,0,0}
-                                };
-         */
-        
-        
-        //Legendario
-          int[][] matrizActual= {
-                                new int[] {0,0,5,3,0,0,0,0,0},
-                                new int[] {8,0,0,0,0,0,0,2,0},
-                                new int[] {0,7,0,0,1,0,5,0,0},
-                                new int[] {4,0,0,0,0,5,3,0,0},
-                                new int[] {0,1,0,0,7,0,0,0,6},
-                                new int[] {0,0,3,2,0,0,0,8,0},
-                                new int[] {0,6,0,5,0,0,0,0,9},
-                                new int[] {0,0,4,0,0,0,0,3,0},
-                                new int[] {0,0,0,0,0,9,7,0,0}
-                                };
-         
-        
-     return matrizActual;    
-    }
+//    public int[][] cargarMatriz()
+//    {
+//        //Facil
+//         /*
+//        int[][] matrizActual= {
+//                                new int[] {8,0,3,9,0,0,1,0,6},
+//                                new int[] {0,6,0,3,0,4,0,0,0},
+//                                new int[] {0,5,4,7,1,0,0,0,0},
+//                                new int[] {0,4,2,0,5,0,0,0,1},
+//                                new int[] {0,3,1,4,0,2,6,5,0},
+//                                new int[] {9,0,0,0,6,0,2,3,0},
+//                                new int[] {0,0,0,0,4,1,8,6,0},
+//                                new int[] {0,0,0,5,0,8,0,9,0},
+//                                new int[] {3,0,8,0,0,9,4,0,5}};
+//                 */
+//         
+//        //Dificil
+//        /*
+//          int[][] matrizActual= {
+//                                new int[] {0,0,0,7,0,0,0,8,0},
+//                                new int[] {2,0,0,0,0,3,0,0,0},
+//                                new int[] {0,4,0,0,0,0,2,0,3},
+//                                new int[] {7,0,0,0,0,0,0,0,6},
+//                                new int[] {0,8,5,0,0,0,0,0,0},
+//                                new int[] {0,0,4,0,8,0,3,7,1},
+//                                new int[] {0,7,0,0,6,8,0,0,9},
+//                                new int[] {0,0,3,0,0,0,4,0,0},
+//                                new int[] {0,0,2,0,5,1,0,0,0}};
+//                */
+//        
+//        /*
+//        //Muy muy dificil
+//          int[][] matrizActual= {
+//                                new int[] {0,9,0,0,0,1,5,0,7},
+//                                new int[] {0,0,0,0,5,0,0,9,0},
+//                                new int[] {0,0,8,9,0,0,2,0,0},
+//                                new int[] {0,0,9,0,0,0,0,4,0},
+//                                new int[] {0,2,0,0,8,0,0,6,0},
+//                                new int[] {0,6,0,0,0,0,1,0,0},
+//                                new int[] {0,0,2,0,0,9,3,0,0},
+//                                new int[] {0,7,0,0,4,0,0,0,0},
+//                                new int[] {1,0,3,5,0,0,0,8,0}
+//                                };
+//        */
+//        
+//        /*
+//        //Endiablado
+//          int[][] matrizActual= {
+//                                new int[] {0,0,0,0,5,0,0,0,2},
+//                                new int[] {7,0,0,0,4,6,0,8,0},
+//                                new int[] {0,6,0,3,8,0,0,9,0},
+//                                new int[] {0,1,0,0,9,5,0,0,4},
+//                                new int[] {0,0,0,0,3,0,0,2,0},
+//                                new int[] {0,0,0,0,1,8,0,0,9},
+//                                new int[] {3,0,0,9,7,0,0,0,8},
+//                                new int[] {0,0,5,0,2,0,0,0,7},
+//                                new int[] {0,0,8,5,6,0,0,0,0}
+//                                };
+//         */
+//        
+//        
+//        //Legendario
+//          int[][] matrizActual= {
+//                                new int[] {0,0,5,3,0,0,0,0,0},
+//                                new int[] {8,0,0,0,0,0,0,2,0},
+//                                new int[] {0,7,0,0,1,0,5,0,0},
+//                                new int[] {4,0,0,0,0,5,3,0,0},
+//                                new int[] {0,1,0,0,7,0,0,0,6},
+//                                new int[] {0,0,3,2,0,0,0,8,0},
+//                                new int[] {0,6,0,5,0,0,0,0,9},
+//                                new int[] {0,0,4,0,0,0,0,3,0},
+//                                new int[] {0,0,0,0,0,9,7,0,0}
+//                                };
+//        
+//     return matrizActual;    
+//    }
        
     
     public int[][] copiarMatriz(int[][]matriz)
@@ -117,9 +116,8 @@ public class SudokuIA {
     }
     
     
-    public int[][] aplicarAlgoritmoAvara()
+    public int[][] aplicarAlgoritmoAvara(int[][] matrizInicial)
     {
-      int[][] matrizInicial=cargarMatriz();
       Heuristica heuristicaInicial = heuristicaPosibilidades.filaColumnaCajaNumeroMinimoPosibilidades(matrizInicial);
       Nodo raiz=new Nodo();
       raiz.setMatriz(matrizInicial);
@@ -193,24 +191,24 @@ public class SudokuIA {
       return matrizSolucion;
     }
     
-    public static void main(String[] args) 
-    {
-        SudokuIA sudoku = new SudokuIA(); 
-        int[][] matrizSolucion = sudoku.aplicarAlgoritmoAvara();
-        
-        String a="";
-        System.out.println(a); 
-        a="";
-        for(int i=0;i<9;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                a+=" "+matrizSolucion[i][j];
-            }
-            System.out.println(a); 
-            a="";
-        }
-       
-    }
+//    public static void main(String[] args) 
+//    {
+//        SudokuIA sudoku = new SudokuIA(); 
+//        int[][] matrizSolucion = sudoku.aplicarAlgoritmoAvara();
+//        
+//        String a="";
+//        System.out.println(a); 
+//        a="";
+//        for(int i=0;i<9;i++)
+//        {
+//            for(int j=0;j<9;j++)
+//            {
+//                a+=" "+matrizSolucion[i][j];
+//            }
+//            System.out.println(a); 
+//            a="";
+//        }
+//       
+//    }
     
 }
